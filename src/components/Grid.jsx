dbd783614dc51ac49d5699ff9fd55d4f1625696f
ben_cripps <ben@benjamincripps.com>
2016-11-23 10:40:38 -0600

    update dev dependencies, remove babel

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-bind */
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import TableContainer from './layout/TableContainer';
