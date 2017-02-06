be37858ad36bc7dd67752a6fd1ae39cbd64b9d83
ben_cripps <ben@benjamincripps.com>
2017-02-06 10:36:28 -0600

    addition of redux-logger, add to confureStore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 
 export default function configureStore(initialState) {
+    const logger = createLogger();
     const createStoreWithMiddleware = applyMiddleware(
-        thunk
+        thunk,
+        logger
     )(createStore);
     const store = createStoreWithMiddleware(rootReducer, initialState);
 
